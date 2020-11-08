8cff17e3e563c5aad21f43d10735a9abfb9e0af7
rustuefegun <74139776+rustuefegun@users.noreply.github.com>
2020-11-08 15:53:35 +0300

    Add files via upload

---
+++ Calculator Pro 2020.py
@@ -0,0 +1,17 @@
+num1 = float(input("Say your first number bro: "))
+op = input("Say your sign bro: ")
+num2 = float(input("Say your second number bro: "))
+
+if op == "+":
+    print("Our result is", num1+num2, "buddy")
+elif op == "_":
+    print("Our result is", num1-num2, "buddy")
+elif op == "*":
+    print("Our result is", num1*num2, "buddy")
+elif op == "/":
+    print("Our result is", num1/num2, "buddy")
+else: print("i dont know man what is that? INVALID")
+
+
+
+
